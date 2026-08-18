ecc1ce5b89431ae0ee9756f29411507c92947d2a
Euclid <euclid_yabut@dlsl.edu.ph>
2026-08-18 20:43:38 +0800

    revert ATTR_EMULATE_PREPARES on PostgreSQL to fix boolean=integer operator error

--- config/database.php
+++ config/database.php
@@ -97,9 +97,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
-            'options' => [
-                PDO::ATTR_EMULATE_PREPARES => true,
-            ],
         ],
 
         'sqlsrv' => [
