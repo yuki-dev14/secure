2558b57462c475e19a7e06923d9ad5a9fdc05b99
Euclid <euclid_yabut@dlsl.edu.ph>
2026-08-18 11:35:23 +0800

    statically bundle Ziggy routes in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from 'ziggy-js';
+import { Ziggy } from './ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'SECURE 4Ps';
 
@@ -18,7 +19,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue, window.Ziggy || {})
+            .use(ZiggyVue, Ziggy)
             .mount(el);
     },
     progress: {
