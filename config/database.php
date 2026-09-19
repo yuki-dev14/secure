33d8a866879cefb8ef1cb8e0e042b511f2d9a879
Euclid <euclid_yabut@dlsl.edu.ph>
2026-09-19 08:19:19 +0800

    Use PGSQL_ATTR_DISABLE_PREPARES to prevent prepared statement errors while preserving boolean typing in PostgreSQL

--- config/database.php
+++ config/database.php
@@ -98,7 +98,7 @@ return [
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
             'options' => [
-                \PDO::ATTR_EMULATE_PREPARES => true,
+                \PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
             ],
         ],
 
